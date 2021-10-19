dab7de659ccabe61f9141aed4deb2efe4b4aa9aa
JimmyLiang-lzm <mibemail@163.com>
2021-10-19 20:28:23 +0800

    Change Version to V1.3.20211019.

--- bilidabout.py
+++ bilidabout.py
@@ -262,9 +262,9 @@ class Ui_Form(object):
         self.label_2.setText(_translate("Form", "Release Version:"))
         self.label_3.setText(_translate("Form", "Release Date:"))
         self.label_4.setText(_translate("Form", "Programmer: "))
-        self.lab_version.setText(_translate("Form", "V1.3.20211016"))
-        self.label_6.setText(_translate("Form", "2021/10/16"))
-        self.label_7.setText(_translate("Form", "二代死神"))
+        self.lab_version.setText(_translate("Form", "V1.3.20211019"))
+        self.label_6.setText(_translate("Form", "2021/10/19"))
+        self.label_7.setText(_translate("Form", "二代死神（B站同名）"))
         self.label_8.setText(_translate("Form", "Contact E-mail:"))
         self.label_9.setText(_translate("Form", "mibemail@163.com"))
 import images_dl_rc
